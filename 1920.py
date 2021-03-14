ef50e9dc9ecf121e6590e515256d7e4adec3bbe3
nbbeom <5877177a@naver.com>
2021-03-14 17:41:41 +0900

    feat : code upload

---
+++ 1920.py
@@ -0,0 +1,12 @@
+n,A = input(),{i: 1 for i in map(int , input.split())}
+m = int(input())
+
+# for i in range(m):
+#     if a[i] in x :
+#         print(1)
+#     else :
+#         print(0)
+
+for i in list(map(int,input().split())):
+    print(A.get(i,0))
+    #dict에서 get 을 사용하면 등록이 안된 킹여도 디폴트 값 0 을 출력
